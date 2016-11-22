e1b7ffe6ee2786d6ab6c425858ce755842839322
bredele <olivier.wietrich@gmail.com>
2016-11-22 16:59:34 -0700

    execute callback even if transitions are the same

--- test/gobs.js
+++ test/gobs.js
@@ -5,12 +5,23 @@
 var test = require('tape')
 var gobs = require('..')
 
-test('should execute callback when all transitions occured', assert => {
+test('should execute callback when all (and different) transitions occured', assert => {
   assert.plan(1)
   var i = 0
   var dispatch = gobs(['hello', 'world'], () =>  i++)
   dispatch('hello')
   dispatch('everybody')
   dispatch('world')
+  assert.equal(i, 1)
+})
+
+
+test('should execute callback when all (and same) transitions occured', assert => {
+  assert.plan(1)
+  var i = 0
+  var dispatch = gobs(['hello', 'hello'], () =>  i++)
+  dispatch('hello')
+  dispatch('world')
+  dispatch('hello')
   assert.equal(i, 1)
 })
