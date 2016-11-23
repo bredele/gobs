6bc270e12c6859a2e0722aecc663022da097393e
bredele <olivier.wietrich@gmail.com>
2016-11-22 17:00:53 -0700

    added doc

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
 
+/**
+ * Execute callback on multiple conditions.
+ *
+ * A condition can appear more than one time.
+ *
+ * @param {Array} transitions
+ * @param {Function} cb
+ * @return {Function}
+ * @api public
+ */
+
 module.exports = function(transitions, cb) {
   return function(event) {
     var index = transitions.indexOf(event)
